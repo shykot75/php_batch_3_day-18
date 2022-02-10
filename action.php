b2382188fc58447b9ef4cb76c89c323048be2c19
shykot75 <shykot1198@gmail.com>
2022-02-10 09:10:02 +0600

    class-18

---
+++ action.php
@@ -0,0 +1,45 @@
+<?php
+    require_once ('vendor/autoload.php');
+    use App\classes\Home;
+    use App\classes\PrimeNumber;
+    use App\classes\Series;
+    use App\classes\Register;
+
+    if(isset($_GET['pages'])){
+        if($_GET['pages'] == 'home'){
+            include 'pages/home.php';
+        }
+        elseif($_GET['pages'] == 'series'){
+            include 'pages/series.php';
+        }
+        elseif($_GET['pages'] == 'register'){
+            include 'pages/register.php';
+        }
+
+    }
+
+    elseif(isset($_POST['btn'])){
+
+        $home = new PrimeNumber($_POST);
+        $result = $home->primeNumber();
+        include 'pages/home.php';
+    }
+
+    elseif(isset($_POST['series_btn'])){
+        $series = new Series($_POST);
+        $result = $series->getSeries();
+
+        include 'pages/series.php';
+    }
+    elseif(isset($_POST['register_btn'])){
+        $register = new Register($_POST);
+
+
+        include 'pages/register.php';
+    }
+
+
+    else {
+        $home = new Home();
+        $home->index();
+    }
